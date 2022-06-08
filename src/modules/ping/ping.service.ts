23b6cdcf1bb001be126f31aba02356acdc3adc72
gabriel-oak <gabriel.costa41@fatec.sp.gov.br>
2022-06-08 01:11:40 -0300

    fix vercel file

--- src/modules/ping/ping.service.ts
+++ src/modules/ping/ping.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class PingService {
   ping(): string {
-    return `Hi, I'm online bro: ${new Date().toISOString()}!`;
+    return `Hi, I'm online bro: ${new Date().toISOString()}`;
   }
 }
